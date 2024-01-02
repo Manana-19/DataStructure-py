5d0a2a21ff00b862c02a687532bce7dea5821e93
Manana-19 <Mananjerath9582@gmail.com>
2024-01-02 23:42:39 +0530

    Linked List (2/2)

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from structure.array import Array
 from structure.hash_table import HashTable
 from structure.linked_list import LinkedList
 # This is a test area
-
-    
-
-
 a = LinkedList(10)
 a.append(20)
 a.append(30)
-print(a.append(40))
-print(a.prepend(50))
+a.append(40)
+a.prepend(50)
+print(a.show())
+print(a.insert(15,3))
+print(a.show())
+a.removeValue(30)
+print(a.show())
+a.RemoveIndex(2)
 print(a.show())
--- structure/linked_list.py
+++ structure/linked_list.py
@@ -58,3 +58,73 @@ class LinkedList:
                 nextValue = nextValue['next']
         
         return toShow
+    
+    # Creating an insert funciton to insert the value at a specific index in this linked list
+
+    def insert(self, value, index):
+        
+        # Creating a if-else if- statement to save work in the following cases
+        # - If the index is out of range
+        # - If the index is 0 So we can just use the function prepend
+        # - If the Index is the same length of the data structure so that we can just use the function append
+
+        if (index > self.length):
+            return False
+        elif (index == 0):
+            self.prepend(value)
+        elif (index == self.length):
+            self.append(value)
+        
+        # Now going through the "for loop" with the range(index-1) so that we don't have to work that hard
+        
+        Node = self.head
+        for i in range(index-1):
+            Node = Node['next']
+        
+        # After this, We replace the old value with the new one, then creating a new node containing the old value and it's next value
+        
+        leader = Node
+        leader['next'] = {
+            'value': value,
+            'next': leader['next']
+        }
+        
+        self.length += 1
+
+        return True
+    
+    def removeValue(self, value):
+        
+        theList = self.show()
+
+        index=-1
+        
+        if value not in theList:
+            return False
+        
+        else:
+            index=theList.index(value)
+
+        return self.RemoveIndex(index)
+    
+    def RemoveIndex(self, index):
+        
+        if (index < 0) or (index >= self.length):
+            return False
+        
+        if index == 0:
+            self.head = self.head['next']
+
+        else:
+
+            Node = self.head
+
+            for i in range(index-1):
+                Node = Node['next']
+        
+            Node['next'] = Node['next']['next']
+            
+
+        self.length -= 1
+
+        return True
